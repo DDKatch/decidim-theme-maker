57740d5a82e84b793244a0a45ef79dcf7151b966
Daniil Kachur <kachur.daniil@gmail.com>
2025-09-04 19:16:14 +0400

    Working css live edit

--- app/packs/src/decidim/theme/maker/admin/controllers/live_css_editor_controller.js
+++ app/packs/src/decidim/theme/maker/admin/controllers/live_css_editor_controller.js
@@ -15,7 +15,6 @@ export default class extends Controller {
   }
 
   postCSS(css) {
-    debugger;
     const iframe = this.iframeTarget;
     if (!iframe || !iframe.contentWindow) return;
     iframe.contentWindow.postMessage({ type: "live-css", css }, "*");
